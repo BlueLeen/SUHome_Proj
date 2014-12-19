4358e83fbbce1670a65294eb27b310c76aeabceb
su <su@su-Ubuntu-Kylin-Net>
2014-12-19 15:50:48 +0800

    revices the CommuClient to connect the android phone and any computer

--- CommuClient/src/CommuClient.cpp
+++ CommuClient/src/CommuClient.cpp
@@ -7,16 +7,23 @@
 //============================================================================
 
 #include <iostream>
+#include <limits.h>
 #include "SocketSeal.h"
+#include "RegTool.h"
 using namespace std;
 
-
-#define MINSIZE  100
+#define TINYSIZE 100
+#define MINSIZE  300
 #define SOCKET_RECVPACK_CONTENT	512
 #define ADB_TCP_SERVER_PORT 7100
 #define SOCKET_SELECT_OPTION "Please select options: 1.send packets 2.receive packets 3.exit!\n"
 #define SOCKET_SEND_COMMENTS "Edit the packet--The packet format is \"code content\" Stop send packet write 0\n"
 #define SOCKET_RECV_COMMENTS "Start receive the packete(If want to stop the receive,please send packet'code equal 0 or 90)--The packet is:\n"
+
+#define SETTINGFILE "setting.ini"
+#define  INFO  "info"
+#define  SVRIP "svrip"
+#define  PORT "port"
 
 int global_client_sockfd = 0;
 
@@ -85,10 +92,18 @@ static void* pthread_func_recv(void* pSockClt)
 }
 
 int main() {
+	char szPath[PATH_MAX] = { 0 };
+	char* curdir = get_current_dir_name();
+	sprintf(szPath, "%s/%s", curdir, SETTINGFILE);
 	char buf[BUFSIZ] = { 0 };
     printf("connected to server\n");
 
-	start_client_socket("127.0.0.1", ADB_TCP_SERVER_PORT, &global_client_sockfd);
+    char szIp[TINYSIZE] = { 0 };
+    RegTool::GetPrivateProfileString(INFO, SVRIP, szIp, szPath,  "127.0.0.1");
+    int port;
+    RegTool::GetPrivateProfileInt(INFO, PORT, port, szPath, ADB_TCP_SERVER_PORT);
+
+	start_client_socket(szIp, port, &global_client_sockfd);
 	//send_socket_packs(SOCKET_STATR_TOKEN, strlen(SOCKET_STATR_TOKEN)+1, global_client_sockfd);
 	receive_socket_packs(buf, BUFSIZ, global_client_sockfd);
 	extract_pack(buf);
---
+++ CommuClient/src/RegTool.cpp
@@ -0,0 +1,111 @@
+/*
+ * RegTool.cpp
+ *
+ *  Created on: Jun 23, 2014
+ *      Author: leen
+ */
+
+#include "RegTool.h"
+#include "inifile.h"
+#include <stdlib.h>
+#include <string.h>
+
+RegTool::RegTool() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RegTool::~RegTool() {
+	// TODO Auto-generated destructor stub
+}
+
+bool RegTool::WritePrivateProfileInt(char* lpAppName, char* lpKeyName, int Value, char* lpFileName)
+{
+	char szValue[20] = { 0 };
+	sprintf(szValue, "%d", Value);
+	write_profile_string(lpAppName, lpKeyName, szValue, lpFileName);
+	return true;
+}
+
+bool RegTool::GetPrivateProfileInt(char* lpAppName, char* lpKeyName, int& Value, char* lpFileName, int ValueDefault)
+{
+	Value = GetIniKeyInt(lpAppName, lpKeyName, lpFileName);
+	//Value = read_profile_int(lpAppName, lpKeyName, ValueDefault, lpFileName);
+	return true;
+}
+
+bool RegTool::WritePrivateProfileString(char* lpAppName, char* lpKeyName, char* Value, char* lpFileName)
+{
+	write_profile_string(lpAppName, lpKeyName, Value, lpFileName);
+	return true;
+}
+
+bool RegTool::GetPrivateProfileString(char* lpAppName, char* lpKeyName, char* Value, char* lpFileName, char* lpDefaultValue)
+{
+	char* szTmp = GetIniKeyString(lpAppName, lpKeyName, lpFileName);
+	strcpy(Value, szTmp);
+//	read_profile_string(lpAppName, lpKeyName, Value, 400, lpFileName);
+	return true;
+}
+
+//从INI文件读取字符串类型数据
+char* RegTool::GetIniKeyString(char *title, char *key, char *filename) {
+	FILE *fp;
+	char szLine[1024] = { 0 };
+	static char tmpstr[1024];
+	int rtnval;
+	int i = 0;
+	int flag = 0;
+	char *tmp;
+	if ((fp = fopen(filename, "r")) == NULL) {
+		printf("have   no   such   file \n");
+		return "";
+	}
+	while (!feof(fp)) {
+		rtnval = fgetc(fp);
+		if (rtnval == EOF) {
+			break;
+		} else {
+			szLine[i++] = rtnval;
+		}
+		if (rtnval == '\n') {
+//#ifndef WIN32
+//			i--;
+//#endif
+			szLine[--i] = '\0';
+//			if(szLine[--i] == '\r')
+//				szLine[i] = '\0';
+			i = 0;
+			tmp = strchr(szLine, '=');
+			if ((tmp != NULL) && (flag == 1)) {
+				if (strstr(szLine, key) != NULL) {
+//注释行
+					if ('#' == szLine[0]) {
+					} else if ('/' == szLine[0] && '/' == szLine[1]) {
+					} else {
+//找打key对应变量
+						strcpy(tmpstr, tmp + 1);
+						fclose(fp);
+						return tmpstr;
+					}
+				}
+			} else {
+				strcpy(tmpstr, "[");
+				strcat(tmpstr, title);
+				strcat(tmpstr, "]");
+				if (strncmp(tmpstr, szLine, strlen(tmpstr)) == 0) {
+//找到title
+					flag = 1;
+				}
+			}
+		}
+	}
+	fclose(fp);
+	return "";
+}
+
+//从INI文件读取整类型数据
+int RegTool::GetIniKeyInt(char *title, char *key, char *filename) {
+	return atoi(GetIniKeyString(title, key, filename));
+}
+
